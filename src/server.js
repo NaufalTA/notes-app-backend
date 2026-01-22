09b1bd0299c726988f07e3af3c92332d337cd629
NaufalTA <Naufallopayy25@gmail.com>
2026-01-23 00:12:58 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 3000;
 const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use("/", routes);
 
 app.listen(port, () => {
