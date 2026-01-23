d1cc99d83bd83c52e2cbcae0462c7dd3ed7cd7f7
NaufalTA <Naufallopayy25@gmail.com>
2026-01-23 16:45:54 +0700

    fix cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "../routes/index.js";
 import ErrorHandler from "../middlewares/error.js";
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
